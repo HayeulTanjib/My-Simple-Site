abbee7f239fa18f1d3ce658936b621a0902c5edb
Hayeul Tanjib Rupok <oniket77@gmail.com>
2020-09-19 11:27:00 +0600

    added files for the site

---
+++ js/mytypejs.js
@@ -0,0 +1,19 @@
+var typed = new Typed('.nameText', {
+  
+  strings: [
+
+  'Hayeul Tanjib Rupok',
+  
+
+  ],
+
+  typeSpeed: 50,
+  //backSpeed: 40,
+  startDelay: 500,
+  backDelay: 1500,
+  loop:true,
+  fadeOut: true,
+  fadeOutDelay: 1000,
+  
+
+});
